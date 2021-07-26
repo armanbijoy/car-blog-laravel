deea285f817f71967e0759a5795a2dfc3b42d293
Arman Bijoy <armanbijoy4@gmail.com>
2021-07-26 20:35:40 +0600

    views added

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PagesController;
 
 
-Route::get('/products', [ProductController::class,'index'])->name('products');
+Route::get('/', [PagesController::class,'index']);
+Route::get('/about',[PagesController::class,'about']);
 
