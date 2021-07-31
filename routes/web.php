88919c5d4d865535657e7e44967ba6cae4ac26ac
Arman Bijoy <armanbijoy4@gmail.com>
2021-08-01 02:19:41 +0600

    Views Added

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\CarsController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
-use App\Http\Controllers\PostController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
 
-Route::get('/', [PagesController::class,'index']);
-Route::get('/about',[PagesController::class,'about']);
-Route::get('/posts',[PostController::class,'index']);
-
-
+Route::resource('/', CarsController::class);
