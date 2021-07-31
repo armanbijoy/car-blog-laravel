84376582f17f4cb67af5fda30100e0ecd49e4b87
Arman Bijoy <armanbijoy4@gmail.com>
2021-07-31 21:47:04 +0600

    added

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 
 
 Route::get('/', [PagesController::class,'index']);
 Route::get('/about',[PagesController::class,'about']);
+Route::get('/posts',[PostController::class,'index']);
+
 
